eb7ba3b66098ba44bacf9d754a2132abb17c7677
sungjkim34 <sungjkim34@gmail.com>
2018-01-06 04:12:56 -0500

    add links to source

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -8,7 +8,7 @@ class About extends Component {
         return (
             <div className="about-container" style={{paddingTop: '75px'}}>
                 <Header as='h2'>About</Header>
-                <p>jThirteen is a project started late night on January 4th, 2018.</p>
+                <p>jThirteen is a project started late night(~1am) on January 4th, 2018.</p>
                 <p>It is a remake of a popular Vietnamese card game called 'Tien Len' or 'Thirteen' in English.</p>
                 <Header as='h3'>Developers</Header>
                 <Card.Group>
@@ -17,6 +17,21 @@ class About extends Component {
                             <Card.Header>Sungjae Kim</Card.Header>
                             <Card.Meta>Developer</Card.Meta>
                             <Card.Description as='a' href='http://sungjkim.com'>sungjkim.com</Card.Description>
+                        </Card.Content>
+                    </Card>
+                </Card.Group>
+                <Header as='h3'>Source/Contribute</Header>
+                <Card.Group>
+                    <Card href='https://github.com/sungjkim34/jthirteenclient'>
+                        <Card.Content>
+                            <Card.Header>Client Code</Card.Header>
+                            <Card.Description>jthirteenclient</Card.Description>
+                        </Card.Content>
+                    </Card>
+                    <Card href='https://github.com/sungjkim34/jthirteenserver'>
+                        <Card.Content>
+                            <Card.Header>Server Code</Card.Header>
+                            <Card.Description>jthirteenserver</Card.Description>
                         </Card.Content>
                     </Card>
                 </Card.Group>
